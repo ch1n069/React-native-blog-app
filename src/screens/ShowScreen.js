dc8b2920419e31c533f7bd053b5836549bdff10e
Bruno Alpachino <brunolalpachino@gmail.com>
2023-04-01 20:20:06 +0300

    extracted params from the route logic

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,10 +1,18 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useContext } from "react";
+import BlogContext from "../context/BlogContext";
 
-const ShowScreen = () => {
+const ShowScreen = ({ route, navigation }) => {
+  // extracting the id from the route.params object
+  const { id } = route.params;
+  //   console.log(id);
+  console.log(id);
+
+  const { state } = useContext(BlogContext);
+  //   const blogPost = state.find((blogPost) => blogPost.id === id);
   return (
     <View>
-      <Text>ShowScreen</Text>
+      <Text></Text>
     </View>
   );
 };
